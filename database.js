ef386a7615c48b768b5428eec1afd49eb2aa93c6
senem35yildirim <senem35yildirim@gmail.com>
2016-01-06 01:58:30 +0200

    :(

---
+++ database.js
@@ -0,0 +1,39 @@
+var catalog = [
+	{
+        "itemName":"Aşk Neden Can Yakar?",
+        "itemAuthor":"Mehmet Yıldız",
+        "itemImage":"foto/kapak.jpg",
+        "itemCreationDate":"2015",
+        "itemDescription":"<p>Ne yaparsan yap olmaz bazen. Ama o kadar güzel olmaz ki, 'Ancak bu kadar güzel olmayabilirdi' dersin. Ve aklına gelir: 'Kadere iman eden kederden emin olur.' Sonra anlarsın ki, nar tanelerini teker teker yerli yerine yerleştiren Rabbin, seni de hangi gönle yerleştireceğini bilir. Tek yapman gereken kara geceleri kudret kalemiyle güneşe boyayan, kahverengi odundan pembe çiçekler açtıran Allah'a inanmak. O'na inanırsan yaklaşmak için tuttuğun elin aslında Allah'tan uzaklaştırdığını anlarsın. O'na inanırsan batıp gidenlerden medet ummaz kalbin esas sahibine yönelirsin. O'na inanırsan 'aşk neden can yakar' anlar ve sabır ipliğiyle diktiğin tüm yaralarını tedavi edersin. İstemez misin kor ateşler etrafını sararken yanmayan bir İbrahim olmayı... <br>(Tanıtım Bülteninden)</p>"
+
+
+    }, 
+	{
+        "itemName":"Seviyorsan Git Konuş Bence",
+        "itemAuthor":"Fatih Yağcı",
+        "itemImage":"foto/fatih.jpg",
+        "itemCreationDate":"2015",
+        "itemDescription":"<p>'Kitap okumayı sevmiyorum ki ben, bunun filmi yok mu?'diyecek olanlara, bu hayat filminin yalnız bir kez çekileceğini ve aslında bu filmin başrol oyuncusunun kendisi olduğunu hatırlatan bu kitabı, önce kendinize, daha sonra bittiği gibi sevdiğiniz bütün namaz kılmakta zorlanan arkadaşlarınıza vermeye can attığınızı daha ilk sayfalarda hissedeceksiniz. Sempatik ve sıra dışı anlatımıyla, gülerken duygulandığın birkaç saat... Âlemlerin Rabbine olan samimiyet ve aşkının şiddetini arttıracak birkaç ipucu... Manevi atmosferinin güçlenerek ibadetlerinde huşu ve huzuru yakalamana vesile birkaç satır...<br>(Tanıtım Bülteninden)</p>"
+    }, 
+	{
+        "itemName":"İçiyorsak Sebebi Var",
+        "itemAuthor":"Harun Serkan Aktaş",
+        "itemImage":"foto/image.jpg",
+        "itemCreationDate":"2015",
+        "itemDescription":"<p>Derdin hep yanında ise şükret!Derdin derman olduğu gibi aynı zamanda dosttur sana… Ve hep acıları söyler.Gecenin en demli anında gelir, çayıma ortak olur. Ve sorar: - Nedendir gece gece bu kadar çay? Çayımdan bir yudum alıp masaya bırakmamdan alır cevabını: - İçiyorsak sebebi var dost… Eminim kitabı okuyan her genç' Benim de bir derdim var ve içiyorsam sebebi var' diyecek.O dert ki, bir davanın kimliği ve ebedi hayatın anahtarı… Satır aralarında ilerledikçe iman gözlüğünü takıp kâinatı seyretmenin huzuruna varacak… Ve Kur'an eczanesinden bu asrın manevi hastalıklarına şifalar sunan 'kırmızı kitaplar'a aşkını tazeleyecek.<br>(Tanıtım Bülteninden)</p>"
+    },
+    {
+        "itemName":"Aşık Da Mı Olmayalım?",
+        "itemAuthor":"Osman Sungur Yeken",
+        "itemImage":"foto/osmann.jpg",
+        "itemCreationDate":"2015",
+        "itemDescription":"<p>Bazen çok sevdiğin bir şey imtihanın olur… Bin bir ümitle, bin bir hayalle çıktığın yolda hüsran dolar avuçlarına… Kâh kendine kızarsın kâh kendinden eksiltenlere… Unutmak istersin, canın yanar… Bırakıp gitmek istersin, ruhun yanar… Artık sevmemek istersin, kalbin yanar… Nereye gitsen seninle gelen hatıraların gitmene izin vermez, gittiğini zannetsen de… Aşk artık acıtmaya, giydiğin bu ateşten gömlek sineni yakmaya, gözyaşların yüreğinden dolup taşmaya başladığı anda ellerini semaya kaldırıp sessizce haykırırsın: 'Ben nerede yanlış yaptım?" "Âşık da mı Olmayalım?' aradığın cevabı bulduracak satırlara davet ediyor seni… <br>(Tanıtım Bülteninden)</p>"
+    },
+    {
+     "itemName":"Benim Olağanüstü Akıllı Arkadaşım",
+        "itemAuthor":"Elena Ferrante",
+        "itemImage":"foto/arkadas.jpg",
+        "itemCreationDate":"2015",
+        "itemDescription":"<p>Kimliğini ısrarla gizlemesiyle adeta bir efsane haline gelen müstear isimli Elena Ferrante'nin dört ciltten oluşan 'Napoli Romanları', sadece İtalya'da değil tüm dünyada fenomen olarak kısa sürede 22 dile çevrildi. Bugün milyonlarca okurun hayranlıkla okuduğu dizinin devam kitaplarının da en kısa sürede raflardaki yerini alacağını belirtiriz. 'Sen benim olağanüstü akıllı arkadaşımsın, hepimizden çok daha başarılı olmalısın, bütün kızlardan ve erkeklerden.' Benim Olağanüstü Akıllı Arkadaşım, İtalya'da bir kenar mahallede yetişen iki genç kızın çekişmeler, kıskançlıklar ve sırlarla örülü dostluklarını, zorluklarla geçen büyüme ve varoluş serüvenlerini anlatıyor. 'Napoli Romanları'nın ilki 50'lerde, fakir bir mahallede başlıyor. Bu unutulmaz dostluk hikâyesinde fazlasıyla akıllı ve duyarlı iki genç kız, Lenù ile Lila, boğucu erkek-egemen kültür, duyarsız, buyurgan aileleri ve yoksunluklar karşısında birbirlerinde teselli bulur. Ancak bu iki sıradışı arkadaş büyüdükçe, onlara dayatılan değerleri kabule yanaşmayacak, büyük fedakârlıklar da gerektirse, birer kadın olarak tutkularını yaşamak ve yaratıcı olmak için ellerinden geleni yapacaktır… 'Benim Olağanüstü Akıllı Arkadaşım sürükleyici, kalabalık, geniş bir 'olgunlaşma romanı.'' -James Wood, The New Yorker- 'Elena Ferrante: öfkeli kadın yazarların en iyisi!' -John Waters, yönetmen- Ferrante'nin genç kızlık ve arkadaşlık meselesini ele alışı olağanüstü etkileyici.- Gwyneth Paltrow, oyuncu<br>(Tanıtım Bülteninden)</p>"
+    }
+];
